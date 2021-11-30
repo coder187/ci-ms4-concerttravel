c48901636a7ae368a62bb24ccf6c1aca13d0c9c8
coder187 <coder187@hotmail.com>
2021-11-30 09:57:18 +0000

    add eventlistadmin class to admin.py

--- events/admin.py
+++ events/admin.py
@@ -2,8 +2,21 @@ from django.contrib import admin
 
 from .models import EventList, EventType, PickLoc, Destination
 
+
+
+
+class EventListAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'event_date',
+        'description',
+        'event_dest',
+    )
+
+    ordering = ('event_date','name')
+
 # Register your models here.
-admin.site.register(EventList)
+admin.site.register(EventList, EventListAdmin)
 admin.site.register(EventType)
 admin.site.register(PickLoc)
 admin.site.register(Destination)
