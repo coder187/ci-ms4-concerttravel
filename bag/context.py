4a8e419e02ce083d7e2ba90e69c38e9058088fbb
coder187 <coder187@hotmail.com>
2021-12-01 11:27:40 +0000

    added logic to bag context processor to return bag contents

--- bag/context.py
+++ bag/context.py
@@ -1,12 +1,36 @@
-
+from events.models import EventList, PickLoc
+from django.shortcuts import get_object_or_404
 
 def bag_contents(request):
-    
-    bag_items =  [] # empty list
 
+    eventid = 0
+    picklocid = 0
+    ticket_price = 0
+    total = 0
+    product_count = 0
+    bag_items = [] 
+    bag = request.session.get('bag', {})
+
+    for event_id, quantity in bag.items():
+        eventid = event_id.split(':')[0]
+        picklocid = event_id.split(':')[1]
+        pickloc_model = get_object_or_404(PickLoc, pk=picklocid)
+        ticket_price = pickloc_model.fare
+        total = total + ticket_price * quantity
+        product_count += quantity
+        bag_items.append({
+                'event_id': eventid,
+                'pickloc_id': pickloc_model.id,
+                'ticket_quantity': quantity,
+                'price_per_ticket': pickloc_model.fare
+            })
+
+    print(total)
+    print(product_count)
     context = {
-        'bag_items':bag_items
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count
     }
 
     return context
-    
--- bag/views.py
+++ bag/views.py
@@ -9,19 +9,20 @@ def add_to_bag(request, event_id):
     '''Add a ticket for the given event to the bag '''
 
     qty = int(request.POST.get('quantity'))
+    pickloc = request.POST.get('pick-loc')
+    event_pickloc = event_id + ':' + pickloc
     redirect_url = request.POST.get('redirect_url')
 
-    # get the session bag or create one if 
+    # get the session bag or create one if
     # does not exist and initialise it to an empty dict
     bag = request.session.get('bag', {})
     
-    if event_id in list(bag.keys()):
-        bag[event_id] += qty
+    if event_pickloc in list(bag.keys()):
+        bag[event_pickloc] += qty
     else:
-        bag[event_id] = qty  # event_id:qty
+        bag[event_pickloc] = qty  # 'event_id:pickloc':qty
 
     request.session['bag'] = bag
-    print(request.session['bag'])
+    # print(request.session['bag'])
     return redirect(redirect_url)
-
 
