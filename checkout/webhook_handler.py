f98175e5d3a6120c4a2aebb2962f3e2c89af2eaf
coder187 <coder187@hotmail.com>
2021-12-07 13:01:23 +0000

    added save oder logic to StripeWH_Handler Class

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
-from events.models import EventList
+from events.models import EventList, PickLoc
 
 import json
 import time
@@ -25,6 +25,95 @@ class StripeWH_Handler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         print(event.data.object)
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        intent = event.data.object
+        pid = intent.id
+        bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
+
+        # Clean data in the shipping details
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
+        order_exists = False
+        attempt = 1
+        while attempt <= 5:
+            try:
+                # ieact exact match case insensitive
+                order = Order.objects.get(
+                    full_name__iexact=shipping_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
+                    grand_total=grand_total,
+                    original_bag=bag,
+                    stripe_pid=pid,
+                )
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        if order_exists:
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                status=200)
+        else:
+            order = None
+            try:
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
+                    original_bag=bag,
+                    stripe_pid=pid,
+                )
+                for item_id, item_data in json.loads(bag).items():
+                    
+                    event_id = item_id.split(':')[0]
+                    pickloc_id = item_id.split(':')[1]
+                    pickloc_model = PickLoc.objects.get(id=pickloc_id) 
+                    fare = pickloc_model.fare
+                    eventlist_model = EventList.objects.get(id=event_id)
+                    qty = int(item_data)
+                
+                    i=0
+                    for i in range(qty):
+                        order_line_item = OrderLineItem(
+                        order=order,
+                        event=eventlist_model,
+                        pickloc=pickloc_model,
+                        price=fare
+                        )
+                    order_line_item.save()
+                    i +=1
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            status=200)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
