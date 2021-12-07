ab462f5cd1b1f135f4147c2a51f0e2cdaf76f76c
coder187 <coder187@hotmail.com>
2021-12-07 15:01:07 +0000

    remove order total from order on webhook  handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -57,7 +57,6 @@ class StripeWH_Handler:
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
                     county__iexact=shipping_details.address.state,
-                    order_total=order_total,
                     original_bag=bag,
                     stripe_pid=pid,
                 )
