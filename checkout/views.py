c756e11dc3e0b3c42ccfa633a62f68ac601b7aa4
coder187 <coder187@hotmail.com>
2021-12-07 00:39:31 +0000

    added stripe payement indent. added loop to add bag items to db

--- checkout/views.py
+++ checkout/views.py
@@ -7,18 +7,59 @@ from .forms import OrderForm
 
 from bag.context import bag_contents
 
+from events.models import PickLoc
+
+import datetime
+
 import stripe 
 
 def checkout(request):
 
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    print('keys:') 
-    print(stripe_public_key)
-    print(stripe_secret_key)
-    
-
+    #print('keys:') 
+    #print(stripe_public_key)
+    #print(stripe_secret_key)
     bag = request.session.get('bag', {})
+
+    if request.method == 'POST':
+        print('POST VALIDATNG FORM')
+        print(datetime.datetime.now())
+   
+        # capture order header from the form
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+
+            # iterate over the bag items and add to db
+            # item_id = event_id:pickloc_id, item_data = qty
+            for item_id, item_data in bag.items():
+                event_id = item_id.split(':')[0]
+                pickloc_id = item_id.split(':')[1]
+                picklocs =PickLoc.objects.get(id=pickloc_id) 
+                fare = picklocs.fare
+                qty = int(item_data)
+    
+                for i in range(qty):
+                    print(f"event id:{event_id} pickloc_id:{pickloc_id}")
+                    print(f"ticket no: {item_data}")
+                    print(f"fare: {fare}")
+
+                    fare
+                    i +=1
+
     if not bag:
         # messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('events'))
@@ -28,18 +69,20 @@ def checkout(request):
     stripe_total = round(total * 100)
 
     stripe.api_key = stripe_secret_key
-    #intent = stripe.PaymentIntent.create(
-    #        amount=stripe_total,
-    #        currency=settings.STRIPE_CURRENCY,
-    #        )
-    #print(intent)
+    intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+            )
+    print(intent)
+
+
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51K0ZuLGc2X9Nm4M2l45fWyPvOUNhNfwIvHdIQV2hCcc9uxdBwjRciYsIpUW8KX42QdGdXc9viedqGEF1PBF6GRQ8004qOoKrSs',
-        'client_secret': 'sk_test_51K0ZuLGc2X9Nm4M21aXjR765tUUCNaMyL6CEbVILac15e9plRiszP25DExP0vifSijVkdXD3FLknq0GnD5JjeOoT00QH0dyqT7',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
