279253b4481d6b8de171e681b874af752d901ea5
coder187 <coder187@hotmail.com>
2021-12-07 03:00:30 +0000

    added checkout success html

---
+++ checkout/templatetags/checkout-tools.py
@@ -0,0 +1,41 @@
+from django import template
+from events.models import PickLoc, EventList
+from checkout.models import Order, OrderLineItem
+from django.shortcuts import get_object_or_404
+
+register = template.Library()
+
+@register.filter(name='hello')
+def hello(quantity,price):
+    return 'how you doin?'
+
+
+@register.filter(name='concat_str')
+def concat_str(arg1, arg2):
+    """concatenate arg1 & arg2"""
+    return str(arg1) + ':' + str(arg2)
+
+
+
+@register.simple_tag(name='multiple_args_tag')
+def multiple_args_tag(a, b, c, d):
+   print (a)
+   print (b)
+   print (c)
+   return
+
+@register.simple_tag(name='get_ticket_qty')
+def get_ticket_qty(order_number,event,pickloc):
+    order = get_object_or_404(Order, order_number=order_number)
+    #for item in order.lineitems.all():
+    #    print (item.event.name)
+    #arg_list = [arg.strip() for arg in args.split(',')]
+    #event = arg_list[1]
+    #pickloc = arg_list[2]
+    #print (args)
+    
+    #event = args.split(':')[0]
+    #pickloc = args.split(':')[1]
+    ticket_count= order.lineitems.filter(event=event,pickloc=pickloc).count()
+    # {% get_ticket_qty order.order_number item.event.id item.pickloc.id  %}
+    return ticket_count
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
@@ -47,52 +47,89 @@ def checkout(request):
             # iterate over the bag items and add to db
             # item_id = event_id:pickloc_id, item_data = qty
             for item_id, item_data in bag.items():
-                event_id = item_id.split(':')[0]
-                pickloc_id = item_id.split(':')[1]
-                pickloc_model = PickLoc.objects.get(id=pickloc_id) 
-                fare = pickloc_model.fare
-                eventlist_model = EventList.objects.get(id=event_id)
-                qty = int(item_data)
+                try:
+
+                    event_id = item_id.split(':')[0]
+                    pickloc_id = item_id.split(':')[1]
+                    pickloc_model = PickLoc.objects.get(id=pickloc_id) 
+                    fare = pickloc_model.fare
+                    eventlist_model = EventList.objects.get(id=event_id)
+                    qty = int(item_data)
                 
-                i=0
-                for i in range(qty):
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        event=eventlist_model,
-                        pickloc=pickloc_model,
-                        price=fare
-                        )
-                    order_line_item.save()
+                    i=0
+                    for i in range(qty):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            event=eventlist_model,
+                            pickloc=pickloc_model,
+                            price=fare
+                            )
+                        order_line_item.save()
                     
-                    # print(f"event id:{event_id} pickloc_id:{pickloc_id}")
-                    # print(f"ticket no: {item_data}")
-                    # print(f"fare: {fare}")
+                        # print(f"event id:{event_id} pickloc_id:{pickloc_id}")
+                        # print(f"ticket no: {item_data}")
+                        # print(f"fare: {fare}")
 
-                    i +=1
+                        i +=1
+                except EventList.DoesNotExist:
+                    messages.error(request, (
+                        "One of the events in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
+            
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
 
-    if not bag:
-        # messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('events'))
+    else:
+        print('START STRIPE INTENT')
+        print(datetime.datetime.now())
+        if not bag:
+            # messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('events'))
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
 
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
             )
-    print(intent)
+        print(intent)
 
+        order_form = OrderForm()
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
 
+        return render(request, template, context)
 
-    order_form = OrderForm()
-    template = 'checkout/checkout.html'
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
     context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        'order': order,
     }
 
     return render(request, template, context)
