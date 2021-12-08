bd895c3ed9c90dca26c05d89c7765c91b9f23bf8
coder187 <coder187@hotmail.com>
2021-12-08 16:35:28 +0000

    add date order and date filter to events all_events

--- events/views.py
+++ events/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from .models import EventList, PickLoc
+from datetime import datetime, timedelta
+from django.conf import settings
 # Create your views here.
 
 # Create your views here.
 def all_events(request):
     ''' a view to show all events, inc sorting and search query'''
 
-    events = EventList.objects.all()  # get all events
-  
 
+    # get all events with an event date bewtween todat and today+days_to_show
+    if hasattr(settings, 'DAYS_TO_SHOW'):
+        days_to_show = int(settings.DAYS_TO_SHOW)
+    else:
+        days_to_show = 365
+    
+    from_date = datetime.today().date()
+    to_date = from_date + timedelta(days=days_to_show)
+    print (from_date)
+    print (to_date)
+    
+    events = EventList.objects.filter(
+        event_date__range=[from_date, to_date]).order_by(
+            'event_date') 
+  
     context = {
         'events':events,
     }
