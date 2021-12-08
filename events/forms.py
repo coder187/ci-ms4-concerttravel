1b54934aa1e626c31180e85451804457dfff0344
coder187 <coder187@hotmail.com>
2021-12-08 22:09:50 +0000

    add date picker to event add form

--- events/forms.py
+++ events/forms.py
@@ -4,10 +4,12 @@ from .models import EventList, PickLoc, EventType, Destination
 
 class EventListForm(forms.ModelForm):
 
+    
     class Meta:
         model = EventList
         fields = '__all__'
-
+        
+        
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         picklocs = PickLoc.objects.all()
@@ -16,6 +18,8 @@ class EventListForm(forms.ModelForm):
         
         dest_friendly_names = [(d.id, d.get_friendly_name()) for d in destinations]
 
+        
+        self.fields['event_date'].widget=forms.widgets.DateInput(attrs={'type': 'date'})
         self.fields['event_dest'].choices = dest_friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
