d75081da334bad84426561a1346729b7ef42f276
coder187 <coder187@hotmail.com>
2021-12-09 13:39:36 +0000

    added CustomClearableFileInput Widget

--- events/forms.py
+++ events/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import EventList, PickLoc, EventType, Destination
-
+from .widgets import CustomClearableFileInput
 
 class EventListForm(forms.ModelForm):
 
@@ -8,6 +8,8 @@ class EventListForm(forms.ModelForm):
     class Meta:
         model = EventList
         fields = '__all__'
+
+        image = forms.ImageField(label='image', required=False, widget=CustomClearableFileInput)
         
         
     def __init__(self, *args, **kwargs):
@@ -18,8 +20,8 @@ class EventListForm(forms.ModelForm):
         
         dest_friendly_names = [(d.id, d.get_friendly_name()) for d in destinations]
 
-        
         self.fields['event_date'].widget=forms.widgets.DateInput(attrs={'type': 'date'})
         self.fields['event_dest'].choices = dest_friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+            
