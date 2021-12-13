e598abc789a0d8582323fe56b85e0f74a6090392
coder187 <coder187@hotmail.com>
2021-12-13 19:39:23 +0000

    fix syntax errors caused by code cleanup

--- events/forms.py
+++ events/forms.py
@@ -9,8 +9,7 @@ class EventListForm(forms.ModelForm):
         model = EventList
         fields = '__all__'
 
-    image = forms.ImageField(label='image', required=False,
-                             widget=CustomClearableFileInput)
+    image = forms.ImageField(label='image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
